03fd92c0faebcc3000b1cbc58eced3deb0f7635d
amber-king <amberking@pursuit.org>
2023-08-27 12:12:23 -0400

    added toggle button for dark mode onto site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
 import CrystalList from "./Components/CrystalList";
@@ -9,8 +10,36 @@ import NavBar from "./Components/NavBar";
 import EditCrystal from "./Components/EditCrystal";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("darkMode");
+    setDarkMode(isDarkMode === "true");
+  }, []);
+
+  useEffect(() => {
+    // Apply dark mode CSS class to the root element
+    document.documentElement.classList.toggle("dark-mode", darkMode);
+
+    // Save the dark mode preference to local storage
+    localStorage.setItem("darkMode", darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? "dark-mode" : "light-mode"}`}>
+      <button
+        className={`dark-mode-toggle-button ${
+          darkMode ? "light-mode" : "dark-mode"
+        }`}
+        onClick={toggleDarkMode}
+      >
+        {darkMode ? "Switch to Light Mode 💡" : "Switch to Dark Mode 🌚"}
+      </button>
+
       <Router>
         <main>
           <Routes>
